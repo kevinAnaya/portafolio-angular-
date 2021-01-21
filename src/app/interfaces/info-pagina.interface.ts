fe61a3525a189bdbce0d0eae05dff81541da85f9
Kevin Anaya <kevinanaya278@gmail.com>
2021-01-21 17:49:21 -0500

    seccion 4 finalizada

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}

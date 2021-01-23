787ecae751b830e50b78c08bc65a6e65490be089
Kevin Anaya <kevinanaya278@gmail.com>
2021-01-23 14:28:24 -0500

    fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,22 +9,41 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any = [];
+
 
   constructor( private http: HttpClient) { 
   
-  console.log('Servicio de infoPagina listo');
-  
-  //Leer el archivo JSON
-  this.http.get('assets/data/data-pagina.json')
-  .subscribe( (resp: InfoPagina) => {
-
-    this.cargada = true;
-    this.info = resp;
-    console.log(resp);
-    
-  });
+  this.cargarInfo();
+  this.cargarEquipo();
 
 
   }
+
+
+  private cargarInfo(){
+
+     //Leer el archivo JSON
+     this.http.get('assets/data/data-pagina.json')
+        .subscribe( (resp: InfoPagina) => {
+
+        this.cargada = true;
+        this.info = resp;
+        
+  
+       });
+    }
+
+    private cargarEquipo(){
+
+      //Leer el json de firebase
+      this.http.get('https://angular-html-bb577-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe( (resp: any) => {
+
+          this.equipo = resp;
+          
+
+        } );
+    }
 }
  
